92b0f667348b93947e7d31af6f22c19f3d3e31a8
hachibu <raymondsohn@gmail.com>
2018-09-09 10:15:37 -0700

    Add types to Reader

--- scripts/toy/src/toy/reader.cr
+++ scripts/toy/src/toy/reader.cr
@@ -5,35 +5,24 @@ class Toy::Reader
     @scanner = StringScanner.new(input)
   end
 
-  def error!(message)
-    abort "Reader error: #{message}: '#{@scanner.rest}'"
-  end
-
-  def read
+  def read : Expr
     exprs = [] of Expr
     until @scanner.eos?
-      case
-      when expr = read_expr
+      if expr = read_expr
         exprs << expr
       else
         error!("Unparsable input")
       end
-      skip_ws
     end
     Expr.new(Expr::Type::Module, exprs)
   end
 
-  def read_expr
-    skip_ws
-    read_op || read_id || read_int || read_quote
-  end
-
-  def skip_ws
-    @scanner.skip(/\s+/)
+  def read_expr : Expr?
+    skip_whitespace { read_op || read_id || read_int || read_quote }
   end
 
   macro define_reader(name, type, pattern, action = nil)
-    def read_{{name}}
+    def read_{{name}} : Expr?
       if w = @scanner.scan({{pattern}})
         Expr.new(
           {{type}},
@@ -48,7 +37,7 @@ class Toy::Reader
   end
 
   macro define_nested_reader(name, type, opening, closing)
-    def read_{{name}}
+    def read_{{name}} : Expr?
       opening_re = Regex.new(Regex.escape({{opening}}))
       closing_re = Regex.new(Regex.escape({{closing}}))
       if @scanner.scan(opening_re)
@@ -73,4 +62,15 @@ class Toy::Reader
   define_reader(id, Expr::Type::Identifier, /[_a-zA-Z][_a-zA-Z0-9]*/)
   define_reader(int, Expr::Type::Integer, /\d+/, to_i)
   define_nested_reader(quote, Expr::Type::Quote, "[", "]")
+
+  def skip_whitespace : Expr?
+    @scanner.skip(/\s+/)
+    value = yield
+    @scanner.skip(/\s+/)
+    value
+  end
+
+  def error!(message) : NoReturn
+    abort "Reader error: #{message}: '#{@scanner.rest}'"
+  end
 end
