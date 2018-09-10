b79ec38e0eb40ec016588e0bd37fe507344ff43e
hachibu <raymondsohn@gmail.com>
2018-09-10 08:58:52 -0700

    Refactor Toy pretty print using surround

--- scripts/toy/src/toy/syntax/expr.cr
+++ scripts/toy/src/toy/syntax/expr.cr
@@ -2,45 +2,40 @@ require "colorize"
 
 module Toy::Syntax
   class Expr
-    def pretty_print(printer)
-      type_color, value_color = [:yellow, :magenta]
-      brack_color, paren_color = [:green, :cyan]
-
-      printer.text("(".colorize(paren_color))
-      printer.text(
-        self.class.to_s.sub("Toy::Syntax::", "").colorize(type_color)
-      )
-      printer.text(" ")
-
-      case self
-      when Identifier, Integer, Operator
-        if self.responds_to?(:value)
-          printer.text(self.value.colorize(value_color))
-        end
-      when Module, Quote
-        printer.nest do
-          printer.list(
-            "[".colorize(brack_color),
-            self.value,
-            "]".colorize(brack_color)
-          )
-        end
-      else
-        error!("#pretty_print: Unhandled case.")
-      end
-
-      printer.text(")".colorize(paren_color))
-    end
-
-    def error!(message) : NoReturn
-      abort "#{self.class}: #{message}"
-    end
-
     macro subclass(name, value_type)
       class Toy::Syntax::{{name}} < Toy::Syntax::Expr
         property value : {{value_type}}
         def initialize(@value : {{value_type}}); end
       end
+    end
+
+    def pretty_print(printer)
+      type_color, value_color = [:yellow, :magenta]
+      brack_color, paren_color = [:green, :cyan]
+
+      printer.surround("(".colorize(paren_color), ")".colorize(paren_color), nil, nil) do
+        printer.text("#{self.base_class.colorize(type_color)} ")
+        case self
+        when Identifier, Integer, Operator
+          if self.responds_to?(:value)
+            printer.text(self.value.colorize(value_color))
+          end
+        when Module, Quote
+          printer.surround("[".colorize(brack_color), "]".colorize(brack_color), "", nil) do
+            printer.list(nil, self.value, nil)
+          end
+        else
+          error!("#pretty_print: Unhandled case.")
+        end
+      end
+    end
+
+    private def base_class
+      self.class.to_s.sub("Toy::Syntax::", "")
+    end
+
+    private def error!(message) : NoReturn
+      abort "#{self.class}: #{message}"
     end
   end
 
